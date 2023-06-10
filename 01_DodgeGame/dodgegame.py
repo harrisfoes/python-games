3a6d42624e4865fdfe03840987c19ce5e7161ba9
harrisfoes <11287035+harrisfoes@users.noreply.github.com>
2023-06-10 19:34:14 +0800

    first pass yo

---
+++ 01_DodgeGame/dodgegame.py
@@ -0,0 +1,78 @@
+import pygame, random, sys
+from pygame.locals import *
+
+WINDOWWIDTH = 600
+WINDOWHEIGHT = 600
+TEXTCOLOR = (255,255,255)
+BACKGROUNDCOLOR = (0,0,0)
+
+FPS = 40
+
+BADDIEMINSIZE = 10
+BADDIEMAXSIZE = 40
+BADDIEMINSPEED = 1
+BADDIEMAXSPEED = 8
+ADDNEWBADDIERATE = 6
+PLAYERMOVERATE = 5
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+def waitForPlayerToPressKey():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    terminate()
+                return
+
+def playerHasHitBaddie(playerRect, baddies):
+    for b in baddies:
+        if playerRect.colliderect(b['rect']):
+            return True
+    return False
+
+def drawText(text, font, surface, x, y):
+    textobj = font.render(text, 1, TEXTCOLOR)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x,y)
+    surface.blit(textobj, textrect)
+
+pygame.init()
+mainClock = pygame.time.Clock()
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+pygame.display.set_caption('Dodger')
+pygame.mouse.set_visible(False)
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), pygame.FULLSCREEN)
+
+# set up fonts
+font = pygame.font.SysFont(None, 48)
+
+#set up sounds
+#gameOverSound = pygame.mixer.Sound('gameover.wav')
+#pygame.mixer.music.load('background.mid')
+
+# show the "Start" screen
+drawText('Dodger', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3))
+drawText('Press a key to start.', font, windowSurface, (WINDOWWIDTH / 3) - 30, (WINDOWHEIGHT / 3) + 50)
+pygame.display.update()
+waitForPlayerToPressKey()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
