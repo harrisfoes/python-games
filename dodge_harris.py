f91143b63551698a429f7ab4192aa4369df6efa5
harrisfoes <11287035+harrisfoes@users.noreply.github.com>
2023-06-11 20:24:32 +0800

    figuring out player

--- dodge_harris.py
+++ dodge_harris.py
@@ -1,6 +1,6 @@
 #make window - done
-#make player display
-    #make player move
+#make player display - done
+    #make player move - done
 #make baddies appear
     #make baddies move
     #handle creation and deletion of baddies
@@ -13,9 +13,11 @@ from pygame.locals import *
 
 WINDOWWIDTH = 600
 WINDOWHEIGHT = 600
-FPS = 30
+FPS = 40
 PLAYERMOVESPEED = 5
 BACKGROUNDCOLOR = (0,0,0)
+BADDIESIZE = 40
+ADDBADDIERATE = 5
 
 pygame.init()
 mainClock = pygame.time.Clock()
@@ -25,8 +27,11 @@ pygame.display.set_caption('Dodger harris version')
 
 playerImage = pygame.image.load('player.png')
 playerRect = playerImage.get_rect()
+baddieImage = pygame.image.load('baddie.png')
 
 moveUp = moveLeft = moveRight = moveDown = False
+baddies = [];
+baddieAddCounter = 0
 
 while True:
     for event in pygame.event.get():
@@ -58,10 +63,29 @@ while True:
     if moveDown and playerRect.bottom < WINDOWHEIGHT:
         playerRect.move_ip(0, PLAYERMOVESPEED)
 
+    #spawn baddies
+    baddieAddCounter += 1;
+    if baddieAddCounter == ADDBADDIERATE:
+        baddieAddCounter = 0
+        newBaddie = {'rect': pygame.Rect(random.randint(0, WINDOWWIDTH-BADDIESIZE), 0 - BADDIESIZE, BADDIESIZE, BADDIESIZE),
+                    'speed': 5,
+                    'surface': pygame.transform.scale(baddieImage,(BADDIESIZE,BADDIESIZE))
+                    }
+        baddies.append(newBaddie)
+
+    for i in baddies:
+        i['rect'].move_ip(0, i['speed'])
+
+    for b in baddies[:]:
+        if b['rect'].top > WINDOWHEIGHT:
+            baddies.remove(b)
     
     #draw
     windowSurface.fill(BACKGROUNDCOLOR)
     windowSurface.blit(playerImage, playerRect)
+
+    for b in baddies:
+        windowSurface.blit(b['surface'], b['rect'])
 
     pygame.display.update()
     mainClock.tick(FPS)
