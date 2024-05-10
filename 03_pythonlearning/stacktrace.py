3bc7a5c7d8074312dd04cb5d5af8a70c5ca9ce29
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2024-05-11 08:41:33 +0900

    python numbers exercise

---
+++ 03_pythonlearning/stacktrace.py
@@ -0,0 +1,4 @@
+def create_stats_message(strength, wisdom, dexterity):
+    total = strength + widom + dexterity
+    msg = f"You have {strength} strength, {wisdom} winsdom, and {dexterity} for a total of {total} stats."
+    return msg
