1da24f190dcc87d97ef975bbdba1491414b20501
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2024-07-16 12:52:05 +0900

    dict in exercise

--- bookbot/main.py
+++ bookbot/main.py
@@ -1,8 +1,18 @@
 def main():
     book_path  = 'books/frankenstein.txt'
     contents = open_book(book_path)
+
     print(contents)
-    print(count_words(contents))
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{count_words(contents)} words found in the document \n")
+
+    dummy_string = "The quick Brown Fox jumps over the lazy doog"
+    char_dict = count_chars(contents)
+    
+    for key,value in char_dict.items():
+        print(f"The '{key}' character was found {value} times")
+
 
 
 def open_book(path):
@@ -12,6 +22,24 @@ def open_book(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_chars(text):
+    dict = {}
+    txt = text.lower()
+
+    #iterate through the characters in 
+    #for each new character, store it to the dict        
+    #count how many of that character is in text
+
+    for char in txt:
+        if char.isalpha() and char not in dict:
+            dict[char] = txt.count(char)
+
+    return dict
+
+def sort_count_char(dict):
+    return dict["num"]
+
 
 
 main()
